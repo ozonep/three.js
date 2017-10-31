655eb897e4f94f2b082a4d9b8af8dc791d10e600
Ivan Malkov <ozonep@yandex.ru>
2017-10-31 09:48:31 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile();
+});
+
+app.listen(8080);
