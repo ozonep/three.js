69d9fc266a609516d82a8c98e7ef5c9c071159d5
Ivan Malkov <ozonep@yandex.ru>
2017-10-31 23:57:23 +0100

    changed background to smaller

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 200);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 300);
 camera.position.z = 100;
 camera.position.y = 50;
 
